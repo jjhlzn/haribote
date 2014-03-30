7fca6b6aea2455bf07b375513d12644391b66e88
jjhlzn <jinjunhang@hotmail.com>
2014-03-30 15:23:22 +0800

    Revert "去掉一些无用的注释"
    This reverts commit c9b33ece9123b1d7cdb85fa6cf6120cc58f9fb7a.

--- harib28a/testsyscall/testfork.c
+++ harib28a/testsyscall/testfork.c
@@ -6,11 +6,18 @@
 void HariMain(void){
 	int pid;
 	char str[100];
+	
+	sprintf(str,"add of str = %d\n",(int)str);
+	printf(str);
     pid = fork();
+	
 	if (pid != 0) { /* parent process */
 		sprintf(str,"parent is running, child pid:%d\n", pid);
-		printf(str);
+		api_putstr0(str);
+		char bufw[100];
 		int s = -1;
+		//sprintf(str,"addr = %d", &s);
+		//api_putstr0(str);
 		int child_pid = wait(&s);
 		sprintf(str,"child_pid = %d, status = %d\n", child_pid, s);
 		printf(str);
@@ -20,9 +27,17 @@ void HariMain(void){
 		pid = getpid();
 		sprintf(str,"child is running, pid:%d\n", pid);
 		printf(str);
+		//spin("child");
 		char bufw[100];
+		//int n = api_read(fd,bufw,25);
+		//api_putstr0("child said: ");
+		//api_putstr0(bufw);
+		//api_putstr0("\n");
+		//int i=1000000;
+		//while(i--);
 		char ** argv = 0;
 		execv("hello4.hrb", argv);
-		api_end();
+		//while(1);
+		//api_end();
 	}
 }
